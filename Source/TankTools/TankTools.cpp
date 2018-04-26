ea062ed35b8efdd3688dcc9c70c58a2693f10634
StackDoubleFlow <stackdoubleflow@gmail.com>
2018-04-26 17:20:48 -0600

    Initial commit

---
+++ Source/TankTools/TankTools.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTools.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TankTools, "TankTools" );
